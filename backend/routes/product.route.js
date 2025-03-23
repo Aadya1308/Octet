5cc23c6f678ca3b6f2a17d4cbdea957b16187547
Aadya1308 <sreedheraadya22@gmail.com>
2025-03-23 10:58:53 +0530

    Add files via upload

---
+++ backend/routes/product.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+	createProduct,
+	deleteProduct,
+	getAllProducts,
+	getProductsByCategory,
+	
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/category/:category", getProductsByCategory);
+
+router.post("/", protectRoute, adminRoute, createProduct);
+
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
+export default router;
